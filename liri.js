0cc7a4722ab083af3752c882aad27371e2ce3a0c
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2019-12-18 13:02:40 -0800

    concert-this venue name shows, along with formatted time

--- liri.js
+++ liri.js
@@ -1,36 +1,41 @@
+// dotenv passwords
 require("dotenv").config();
 
+// Moment npm package 
+var moment = require('moment');
+
+// fs core package 
 var fs = require('fs');
 
+// axios package 
 var axios = require("axios");
 
+// Spotify package 
 var Spotify = require('node-spotify-api');
 
+// api key for spotify
 var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 
+// Sets process 2 and 3 as shorter arguments 
 var arg2 = process.argv[2];
 var arg3 = process.argv[3];
 
-// Read txt file 
+// Read txt file, run with do-what-it-says
 function readText() {
 
     fs.readFile("random.txt", "utf8", function (error, data) {
-        console.log(data);
         var dataArr = data.split(",");
-        console.log(dataArr[0]);
-        console.log(dataArr[1]);
         switcher(dataArr[0], dataArr[1]);
     });
 };
 // readText();
 
-// Get movie data using axios 
+// Get movie data, run with movie-this "movie"
 function getMovie(movieName) {
 
     var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=full&tomatoes=true&apikey=trilogy";
-    // var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
     axios.get(queryURL).then(
         function (response) {
             console.log(response.data);
@@ -47,10 +52,7 @@ function getMovie(movieName) {
 
 }
 
-// getMovie("spaceballs");
-// Output movie data into terminal
-
-// Start Music Function 
+// Start Music Function, run with 
 function getMusic(songName) {
     // If no song name is input 
     if (!songName) {
@@ -78,20 +80,30 @@ function getMusic(songName) {
     });
 }
 
-// Get concert function 
+// Get concert function, run with concert-this
 function getConcert(bandName) {
     var queryURL = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp";
     axios.get(queryURL).then(
         function (response) {
-            console.log(response.data);
-            // * Title of the movie.
-            // * Year the movie came out.
-            // * IMDB Rating of the movie.
-            // * Rotten Tomatoes Rating of the movie.
-            // * Country where the movie was produced.
-            // * Language of the movie.
-            // * Plot of the movie.
-            // * Actors in the movie.
+            for (let i = 0; i < response.data.length; i++) {
+                const show = response.data[i];
+
+                // Name of venue 
+                console.log("Venue Location: " + show.venue.name);
+                // Date of event 
+                console.log(moment(show.datetime).format("MM/DD/YYYY"));
+                // console.log
+            }
+            // console.log(response.data);
+
+            //  * Name of the venue
+
+            //             * Venue location
+
+            //                 * Date of the Event(use moment to format this as "MM/DD/YYYY")
+
+            // console.log(response.data)
+
         }
     )
 
@@ -118,8 +130,8 @@ function switcher(arg2, arg3) {
             getMovie(arg3);
             break;
         case "concert-this":
-
-
+            getConcert(arg3);
+            break;
     }
 }
 switcher(arg2, arg3);
